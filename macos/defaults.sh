30e8732cf5d8c106a830c701b926e38237ea505b
Rokas Baršauskas <rokas@barsauskas.lt>
2024-02-16 23:14:29 +0200

    defaults improt symbolichotkeys

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -1,3 +1,9 @@
+# Use F1, F2... as standard function keys
+defaults write "Apple Global Domain" "com.apple.keyboard.fnState" "1"
+
+# Import customized keyboard shortcuts
+defaults import com.apple.symbolichotkeys ~/.dotfiles/macos/settings/symbolichotkeys.plist
+
 # Specify the preferences directory
 defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/.dotfiles/iterm2/settings"
 
