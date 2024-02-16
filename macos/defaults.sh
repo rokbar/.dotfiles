0f95c124e82654b657a891344fcbfe367edd8375
Rokas Baršauskas <rokas@barsauskas.lt>
2024-02-16 23:28:53 +0200

    autohide dock

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -9,3 +9,6 @@ defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/.dotfiles/iter
 
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+
+# Autohide dock
+defaults write com.apple.Dock autohide "1"
