eda265d8f69ec5e0cc1399ddcfec0fb829396474
Rokas Baršauskas <rokas@barsauskas.lt>
2026-01-04 14:20:54 +0200

    Remove Nix.

--- nix/home/activation.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, lib, config, ... }:
-{
-  home.activation = {
-    runCustomScript = lib.hm.dag.entryAfter ["writeBoundary"] ''
-        $DRY_RUN_CMD echo "Running custom script"
-        $DRY_RUN_CMD ${config.home.homeDirectory}/.dotfiles/git/gitconfig_setup.sh
-    '';
-  };
-}
